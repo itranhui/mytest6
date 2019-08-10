b2775f669bc7403c1b224bd6f1b97cbcf155e479
ranhui <17725763053@163.com>
2019-08-10 19:07:22 +0800

    第一次提交代码

---
+++ src/main/java/com/itranhui/pojo/User.java
@@ -0,0 +1,12 @@
+package com.itranhui.pojo;
+
+/**
+ * @Author：Mr.ran &Date：2019/8/10 19:02
+ * <p>
+ * @Description：
+ */
+
+public class User {
+    private String name;
+    private  String age;
+}
