90e6ff9c472863990e56b67baf8da53c6e281e08
ranhui <17725763053@163.com>
2019-08-10 20:02:01 +0800

    第一次修改

--- src/main/java/com/itranhui/pojo/Student.java
+++ src/main/java/com/itranhui/pojo/Student.java
@@ -9,4 +9,27 @@ package com.itranhui.pojo;
 public class Student {
 
     private  String  student;
+    /**
+     * U1修改代码
+     *
+     *
+     */
+
+private  String age;
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
